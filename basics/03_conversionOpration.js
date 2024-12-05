a89a4ddf89b55837f1c8d48e83cbe20951adfddf
PRIYANSH GARG <priyanshgarg0001@gmail.com>
2024-12-05 12:29:57 +0000

    conversion dataType

---
+++ basics/03_conversionOpration.js
@@ -0,0 +1,35 @@
+// let score =true
+// let score =false
+// let score =undefined
+// let score =undefined
+
+// console.log(typeof score);
+// console.log(typeof (score)); 
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+// console.log(score);
+
+// "33" => 33
+// "33abc" => nan (not an number)
+// true => 1; false => 0
+
+// let isLoggedIn ="Priyansh"
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+//  1=> true; 0 => false
+// "" => false
+// "Priyansh => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+ 
