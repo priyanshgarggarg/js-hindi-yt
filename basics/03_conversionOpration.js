3f1f592e952d27f6768a9605f60473fabebb9c8e
PRIYANSH GARG <priyanshgarg0001@gmail.com>
2024-12-06 16:22:02 +0000

    conversionOprations

--- basics/03_conversionOpration.js
+++ basics/03_conversionOpration.js
@@ -24,12 +24,48 @@
 // "" => false
 // "Priyansh => true
 
-let someNumber = 33
+// let someNumber = 33
 
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ************************** Operations *************************************
+
+let value = 3
+ let negValue = -value
+//  console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2/2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2%2);
+
+let str1 = "Hello"
+let str2 = " PRIYANSH"
+
+let str3 = str1 + str2
+console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2" + 3 );
+// console.log("1" + "2");
+// console.log("1" + 2 + 3 + "5");
+
+// console.log((3+3) * 52 %2); not to use  
+
+// console.log(true);
+// console.log(+"");
+
+// let num1, num2, num3
+
+// num=1 = num2 = num3 = 2+4
+
+let gameCounter = 100
+// gameCounter++;
+++gameCounter
+console.log(gameCounter);
 
 
-
- 
