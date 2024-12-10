a2f87783b28d8b6cdd0d16e78c713b3980e3b96e
PRIYANSH GARG <priyanshgarg0001@gmail.com>
2024-12-10 14:18:24 +0000

    nums and math

---
+++ basics/07__nums_and_math.js
@@ -0,0 +1,35 @@
+const score = 400
+// console.log(score );
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 2123.67898
+
+// console.log(otherNumber.toPrecision(5));
+
+const presentNumber  = 10000000
+// console.log(presentNumber.toLocaleString('en-IN'));
+ 
+//////////////////////// *MATH* //////////////////////////////
+ 
+// console.log(Math);
+// console.log(Math.abs(-5)); // isse (-) values (+) main print hoti hai !!
+// console.log(Math.round(43.33)); // point vali values ko round off kr deta hai !!
+// console.log(Math.ceil(4.2)); // 4 se 4.2 vali value ko usse upper vali value deta hai !!
+// console.log(Math.floor(4.2));// 4 se 4.2 vali value ko usse lower vali value deta hai !!
+// console.log(Math.min(4,2,5,7)); // isme minium value print karta hai
+// console.log(Math.max(4,2,7,9));// isme maximum value print karta hai 
+
+console.log(Math.random()); // isme random values aati hai as a 0 se badi or 1 se chhoti
+console.log((Math.random()*10)+1);
+console.log((Math.random()*10)+1);
+
+
+const min = 10
+const max = 40
+
+console.log(Math.floor(Math.random()*(max - min + 1)))
